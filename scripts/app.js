2c97387888ae814d3fd410f26b15bc0f57e06da0
Dominik Szojda <szojda.98@gmail.com>
2021-07-20 20:25:45 +0200

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -7,3 +7,26 @@ const mapButtFunction = () => {
 }
 
 mapButt.addEventListener("click", mapButtFunction);
+
+
+
+document.addEventListener('scroll', () => {
+    const logo = document.querySelector('.logo');
+    const lista = document.querySelector('.menu__list');
+    const nav = document.querySelector('.menu');
+    const item = document.querySelector('.menu__list--item');
+    let x = window.scrollY;
+    console.log(x)
+    if (x >= 20) {
+        nav.classList.add('nav__trasform');
+        lista.classList.add('menu__list--transform');
+        logo.classList.add('menu__logo--transform');
+        item.classList.add('menu__item--transform');
+    }
+    else {
+        nav.classList.remove('nav__trasform');
+        lista.classList.remove('menu__item--transform');
+        logo.classList.remove('menu__logo--transform');
+        item.classList.remove('menu__item--transform');
+    }
+});
