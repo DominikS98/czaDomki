d13b7cc5ff45e03bfe1167aaa362f9dfdb2d58a8
Dominik Szojda <szojda.98@gmail.com>
2021-08-01 13:00:34 +0200

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -9,24 +9,47 @@ const mapButtFunction = () => {
 mapButt.addEventListener("click", mapButtFunction);
 
 
+const mobileMenu = document.querySelector('.mobile--i');
+
+const mobileMenuOn = () =>{
+    const menu = document.querySelector('.mobile__menu');
+    menu.classList.toggle('mobileOn');
+}
+
+mobileMenu.addEventListener("click", mobileMenuOn);
 
 document.addEventListener('scroll', () => {
     const logo = document.querySelector('.logo');
     const lista = document.querySelector('.menu__list');
     const nav = document.querySelector('.menu');
     const item = document.querySelector('.menu__list--item');
+    const toUp = document.querySelector('.to__up');
     let x = window.scrollY;
     console.log(x)
     if (x >= 20) {
         nav.classList.add('nav__trasform');
         lista.classList.add('menu__list--transform');
         logo.classList.add('menu__logo--transform');
-       
+        let all = document.getElementsByClassName('menu__list--item');
+        for (let i = 0; i < all.length; i++) {
+            all[i].classList.add('menu__item--transform');
+        }
+
     }
     else {
         nav.classList.remove('nav__trasform');
         lista.classList.remove('menu__item--transform');
         logo.classList.remove('menu__logo--transform');
         item.classList.remove('menu__item--transform');
+        let all = document.getElementsByClassName('menu__list--item');
+        for (let i = 0; i < all.length; i++) {
+            all[i].classList.remove('menu__item--transform');
+        }
+    }
+    if (x >= 7510) {
+        toUp.classList.add('to__up--animation');
+    }
+    else {
+        toUp.classList.remove('to__up--animation');
     }
 });
